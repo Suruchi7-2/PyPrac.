21eacbc1f1c986725fbbc1a759a51fdc2cdff00d
Suruchi7-2 <suruchikumari8579@gmail.com>
2024-03-18 13:21:41 +0530

    commit

--- 1st.py
+++ 1st.py
@@ -1,1 +1,16 @@
-print("hello")
+class Student:
+    def __init__(self,name,address):#use self for attribute it is same as this used to use anothere name of attribute /variabkle in class
+        self.naam=name#like name is defined but we used another name for name as naam.
+        self.pata=address
+        #now add function to defined behaviour of above class like function 
+    def studetail(self):
+        return f"Name: {self.naam}, Address: {self.pata}"
+
+
+
+mystud=Student("Suruchi","Bihar")#instance mystud .when it created above constructor called itsdelf.
+print(mystud.naam)#way to print attribute or variable using dot.
+mystud1=Student("ANupriya","Bihar")
+store= mystud1.studetail()
+print(store)
+
