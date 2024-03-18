bcd4a8bd7dba2c10dab75e69fc9b4a8c87906994
Suruchi7-2 <suruchikumari8579@gmail.com>
2024-03-18 16:57:14 +0530

    Again YAY

--- Encapsulatte1.py
+++ Encapsulatte1.py
@@ -1,6 +1,5 @@
-class Animal:
-    #Encapsulation is to encapsulate data memebr on is own. like private meber is of unction only so noone one can acces it.
-    total=0
+class Animal:#Encapsulation is to encapsulate data memebr on is own. like private meber is of unction only so noone one can acces it.
+    total=0#this is class variable
     def __init__(self,legs1,head1,tail):
         self.legs1=legs1
         self.head1=head1
@@ -10,19 +9,22 @@ class Animal:
         return f"Legs={self.legs1}, Head={self.head1}, Tail={self.__tail}"
     def factor(self):
         return f"Animal is it"
+    @staticmethod 
+    def description():#make it static method so only it can acces through class not by instance.
+        return f"u r static method hence no need of self"
+    
 class Dog(Animal):
     def __init__(self,legs1,head1):
         super().__init__(legs1,head1,tail=2)
     def factor(self):
         return f"DOg is it"
 an=Animal(4,1,1)#so any object can't acces private variable but function have accessability only.
-store=an.detail()
+store=Animal.description()
 print(store)
 print(an.factor())
 #POlymorphism is having multiple form of same things like above function factor in both class, same thing factor is but prpinting differen things.
 d=Dog(2,1)
 store1=d.detail()
 print(store1,d.factor())
-print(Animal.total)
-
-# due to double underscore any variable become private and hence any object if that can't access that private memnber. only class can accessprint(an.__tail)
+#print(Animal.total)
+print(an.description())
