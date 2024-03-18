8b603b34363dd0c0a8acf455eee132105cdff19d
Suruchi7-2 <suruchikumari8579@gmail.com>
2024-03-18 23:18:32 +0530

    inheritance done

---
+++ 3rd.py
@@ -0,0 +1,18 @@
+#Let's PAss emmebr of one class to another class
+#yes, u can do this using passing object of one class as parameter . creatiion process.
+class Student:
+    def __init__(self,n,r):
+        self.name=n
+        self.roll=r
+    def show(self):
+        print("tudent name is ",self.name)
+        print("Student's roll is: ",self.roll)
+#Another class
+class Teacher:
+    @staticmethod
+    def display(s):
+        print("Teacher name is ",s.name)
+        print("Teacher's roll is: ",s.roll)
+stu=Student("Suruchi",12)
+Teacher.display(stu)
+            
---
+++ inheritance.py
@@ -0,0 +1,43 @@
+#whenever we  created classes, so all classeds in python are built from a  single super class that is object object will become super class for them internally.
+#Type of Inheritance
+#1.Single INheritance
+#2.Multilevel Inheritance
+#3.Multipple Inheritance
+#4Hierarchical INehritance
+#if we declare constructor in both classes, then parent class constructor is not acccessible
+#which means only child class constructor is accesible.
+#and the above situation is called constructor overriding.
+#but when u want constructor of parrett class should called then use super method.
+class Father:
+    def __init__(self):
+        print("father class constructor called")
+class Son(Father):
+    def __init__(self):
+        super().__init__()#due to thissuper method in child,now we can acces constructor of parent 
+        print("SOn class constructor called")
+S=Son()
+
+#Multilevel Inheritance.......
+#class Father:
+#class son(Father):
+#class grandson(son)
+#Hierarchical Inheritance.......
+#class father:
+#class son(father):
+#class daughter(father)
+#Multiple Inheritance:
+#class Father:
+#class Mother:
+#class son(Father,Mother)
+class Father:
+    def shof(self):
+        print("Father class")
+class Mother:
+    def showm(self):
+        print("Mother class method")
+class daughter(Father,Mother):
+    def showd(self):
+        print("DAughter called")
+d=daughter()
+d.showd()
+d.shof()
