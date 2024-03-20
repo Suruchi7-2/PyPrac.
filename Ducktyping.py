aff2945313a4d36d5d75d543f2c83e630aa4b0ed
Suruchi7-2 <suruchikumari8579@gmail.com>
2024-03-20 09:19:30 +0530

    Overloading done

---
+++ Ducktyping.py
@@ -0,0 +1,19 @@
+#polymorphism have types:
+#Duck Typing
+#Operator Overloading
+#MEthod Overloading
+#MEthod Overriding
+#...........Duck typing................
+# So, pyhton says,if horse walks like a duck or speak like a duch means it behaviour like a  duck while ther are not duck,yet we will consider horse like a duck
+#means python doesn't care about which class of objecjt is , if it is an object and required behaviour is present for that object then it will work
+#the type of object is  diferenced at runtime only that is duck typing
+class Duck:
+    def walk(self):
+        print("thapak thapakp thapak thapak")
+class Horse:
+    def walk(self):
+        print("tabdak tabdak tabdk tabdak")
+def myfunction(self):
+    self.walk()
+d=Duck()
+myfunction(d)
---
+++ strongtyping.py
@@ -0,0 +1,45 @@
+#hasattr() fnction is used to check whether the object has a method or not,if it's found then return true otherwise false.
+#like  while in ducktyping, there is no ned to check hether that method is prrsent inside  that object clas  or not, but here we chheck
+class Duck:
+    def walk(self):
+        print("duck")
+class Horse:
+    def walk(self):
+        print("Horse")
+def myfunction(self):
+    if(hasattr(self,"walk")):
+         self.walk()
+d=Duck()
+myfunction(d)
+
+#MEthod overloading:Python should not  have same name of emthods in one class, 
+#henc here, method overloading is different from others.
+#IN python,if a method is written in such a way that it can perform more than one task.and that is method overloadinfg
+class Hyy:
+    def sum(self,a=None,b=None,c=None):
+        if a!=None and b!=None and c!=None:
+            s=a+b+c
+        elif a!=None and b!=None:
+            s=a*b
+        else:
+            s="Provide atleast two numbers"
+        return s
+obj=Hyy()
+print(obj.sum(43,12))
+          
+#Method Overridng. MEthod overridng is same as constructor overriding.
+#like for method, metod overriding, base and derived class should be. 
+#like when same namemethod is defined in base and derived , then only derived class method is calling when object of derived class is calling that same method.
+#so, we try to call parent method too ,hence use super(). method.
+
+class Add:#Base class
+    def result(self,x,y):
+        print("Addition: ",x+y)
+class Multi(Add):#Derived class.
+    def result(self,a,b):
+        super().result(10, 20)#due tot his, parent class method also called but always passed actual argument here.
+        print("Multiplication: ",a*b)
+m=Multi()
+m.result(10,20)
+
+#
